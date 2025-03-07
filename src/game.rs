796f9453ca8eedfeb041fa26d49f8d39b9afe43c
Ellen Marie Dash <me@duckie.co>
2025-03-07 18:56:00 -0500

    switch to Bevy for timing and such

--- src/game.rs
+++ src/game.rs
@@ -12,7 +12,7 @@ pub enum Action {
 
 type GridLine = [u8; GRID_WIDTH];
 type Grid = [GridLine; GRID_HEIGHT];
-#[derive(Clone)]
+#[derive(Clone, Component)]
 pub struct Game {
     grid: Grid,
     current_landed: bool,
--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,34 @@ const GRID_FIRST_VISIBLE: usize = PIECE_HEIGHT;
 const GRID_WIDTH: usize = 5;
 const GRID_HEIGHT: usize = 6 + PIECE_HEIGHT;
 
-fn main() {
-    let mut game = Game::default();
+#[derive(Resource)]
+struct GameClock(Timer);
 
-    while !game.stuck() {
-        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-        game.tick(Action::None);
-        game.print();
-        std::thread::sleep(std::time::Duration::from_millis(300));
+fn init(mut commands: Commands) {
+    println!("init()");
+    commands.spawn(Game::default());
+}
+
+fn tick(time: Res<Time>, mut timer: ResMut<GameClock>, mut query: Query<&mut Game>) {
+    // update our timer with the time elapsed since the last update.
+    // if that didn't cause the timer to finish, return immediately
+    if !timer.0.tick(time.delta()).just_finished() {
+        return;
     }
 
-    //App::new().run();
+    println!("tick()");
+    for mut game in &mut query {
+        game.tick(Action::None); // TODO: handle input
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+        game.print();
+    }
+}
+
+fn main() {
+    App::new()
+        .insert_resource(GameClock(Timer::from_seconds(1.0, TimerMode::Repeating)))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, init)
+        .add_systems(Update, tick)
+        .run();
 }
